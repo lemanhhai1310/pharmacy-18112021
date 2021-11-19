764a915adb3ae6fa3fa44c2e64218e8d360c7753
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-11-19 15:28:51 +0700

    feat: add layout header

--- template-parts/layouts/header.php
+++ template-parts/layouts/header.php
@@ -19,7 +19,71 @@
     <script src="js/app.js?v=<?php echo(time()) ?>"></script>
 </head>
 <body class="">
+<?php
+$isLogin = rand(true,false);
+?>
 <?php require "template-parts/commons/facebook_chat.php"; ?>
+<!--menumobile-->
+<div id="offcanvas-menumobile" class="header__offcanvas" uk-offcanvas="overlay: true">
+    <div class="uk-offcanvas-bar">
+        <button class="uk-offcanvas-close header__bottom__close header__bottom__close--text" type="button" uk-close></button>
+        <div class="" style="margin-bottom: 16px">
+            <div class="uk-position-relative header__bottom__search">
+                <input class="uk-input uk-border-pill header__bottom__search__input" type="text" placeholder="Bạn cần tìm gì">
+                <button type="button" class="header__bottom__search__btn uk-button uk-button-default uk-position-center-right"></button>
+            </div>
+        </div>
+        <ul class="header__offcanvas__nav uk-nav-default uk-nav-parent-icon" uk-nav="toggle: .header__offcanvas__nav__arrow">
+            <li class="uk-active"><a href="#">Về chúng tôi</a></li>
+            <li><a href="">Sản phẩm</a></li>
+            <li class="uk-parent">
+                <a href="#">Tin tức</a>
+                <i class="header__offcanvas__nav__arrow"></i>
+                <ul class="uk-nav-sub">
+                    <li><a href="#">Tin hoạt động Poke</a></li>
+                    <li><a href="#">Thông tin y dược</a></li>
+                </ul>
+            </li>
+            <li><a href="">Tuyển dụng</a></li>
+            <li><a href="">Liên hệ</a></li>
+            <li><a href="">FAQ</a></li>
+        </ul>
+        <div class="uk-text-center header__offcanvas__boxLang">
+            <div class="header__top__language" tabindex="0" aria-expanded="true">
+                <span>Tiếng Việt</span>
+            </div>
+            <div class="uk-navbar-dropdown header__dropdown" uk-dropdown="mode: click;pos:bottom-center">
+                <ul class="uk-nav uk-navbar-dropdown-nav">
+                    <li><a href="#">Tiếng Việt</a></li>
+                    <li><a href="#">Tiếng Anh</a></li>
+                    <li><a href="#">Tiếng Nhật</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/menumobile-->
+<!--acc-->
+<div id="offcanvas-acc" class="header__offcanvas" uk-offcanvas="overlay: true;flip: true">
+    <div class="uk-offcanvas-bar">
+        <button class="uk-offcanvas-close header__bottom__close header__bottom__close--text" type="button" uk-close></button>
+        <?php if ($isLogin): ?>
+            <div class="header__offcanvas__user">
+                <span>Anh</span>
+            </div>
+            <ul class="header__offcanvas__nav uk-nav-default uk-nav-parent-icon" uk-nav="toggle: .header__offcanvas__nav__arrow">
+                <li><a href="#">Thông tin tài khoản</a></li>
+                <li><a href="">Đăng xuất</a></li>
+            </ul>
+        <?php else: ?>
+            <ul class="header__offcanvas__nav uk-nav-default uk-nav-parent-icon" uk-nav="toggle: .header__offcanvas__nav__arrow">
+                <li><a href="#">Đăng nhập</a></li>
+                <li><a href="">Đăng ký</a></li>
+            </ul>
+        <?php endif; ?>
+    </div>
+</div>
+<!--/acc-->
 <!--app-->
 <div id="app" class="uk-height-viewport uk-offcanvas-content uk-overflow-hidden uk-position-relative">
 <!--header-->
@@ -76,6 +140,9 @@
             <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
 
                 <div class="uk-navbar-left">
+                    <a href="#offcanvas-menumobile" class="uk-navbar-item uk-hidden@m" uk-toggle>
+                        <div class="header__bottom__box2 header__bottom__box2--menu"></div>
+                    </a>
                     <a href="." class="uk-navbar-item uk-logo"><img src="images/logo_h.png" alt=""></a>
                 </div>
 
@@ -90,20 +157,49 @@
                     <!--/search-->
 
                     <!--quantam-->
-                    <div class="uk-navbar-item">
-
+                    <a href="" class="uk-navbar-item uk-hidden@m">
+                        <div class="header__bottom__box2 header__bottom__box2--wishList"></div>
+                    </a>
+                    <div class="uk-navbar-item header__bottom__marginLeft uk-visible@m">
+                        <div class="header__bottom__box1 header__bottom__box1--wishList">
+                            <div class="header__bottom__box1__txt1"><a class="uk-link-toggle" href="">Quan tâm</a></div>
+                            <div class="header__bottom__box1__txt2">0 sản phẩm</div>
+                        </div>
                     </div>
                     <!--/quantam-->
 
                     <!--giohang-->
-                    <div class="uk-navbar-item">
-
+                    <a href="" class="uk-navbar-item uk-hidden@m">
+                        <div class="header__bottom__box2 header__bottom__box2--cart"></div>
+                    </a>
+                    <div class="uk-navbar-item header__bottom__marginLeft uk-visible@m">
+                        <div class="header__bottom__box1 header__bottom__box1--cart">
+                            <div class="header__bottom__box1__txt1"><a class="uk-link-toggle" href="">Giỏ hàng</a></div>
+                            <div class="header__bottom__box1__txt2">0 sản phẩm</div>
+                        </div>
                     </div>
                     <!--/giohang-->
 
                     <!--account-->
-                    <div class="uk-navbar-item">
-
+                    <a href="#offcanvas-acc" class="uk-navbar-item uk-hidden@m" uk-toggle>
+                        <div class="header__bottom__box2 header__bottom__box2--acc"></div>
+                    </a>
+                    <div class="uk-navbar-item header__bottom__marginLeft uk-visible@m">
+                        <div class="header__bottom__box1 header__bottom__box1--acc">
+                            <div class="header__bottom__box1__txt1"><?= ($isLogin)?'Anh':'Tài khoản' ?></div>
+                            <div class="header__bottom__box1__txt2"><?= ($isLogin)?'Thông tin tài khoản':'Đăng nhập/ đăng ký' ?></div>
+                        </div>
+                        <div class="uk-navbar-dropdown header__dropdown" uk-dropdown="mode: hover">
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                <?php if ($isLogin): ?>
+                                    <li><a href="#">Thông tin tài khoản</a></li>
+                                    <li><a href="#">Đăng xuất</a></li>
+                                <?php else: ?>
+                                    <li><a href="#">Đăng nhập</a></li>
+                                    <li><a href="#">Đăng ký</a></li>
+                                <?php endif; ?>
+                            </ul>
+                        </div>
                     </div>
                     <!--/account-->
                 </div>
