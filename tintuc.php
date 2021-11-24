b428c08aff82d20eff2752bb3cfa99e533ad46e4
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-11-25 00:45:03 +0700

    feat: add page tintuc

--- tintuc.php
+++ tintuc.php
@@ -101,7 +101,24 @@ require "template-parts/layouts/breadcrumb.php";
                 </div>
             </div>
             <div class="uk-width-1-4@m">
-
+                <div class="uk-card tintuc__asideCard">
+                    <div class="tintuc__asideCard__item">
+                        <div class="uk-position-relative tintuc__asideCard__search">
+                            <input class="uk-input tintuc__asideCard__search__input" type="text" placeholder="Tìm kiếm">
+                            <button type="button" class="tintuc__asideCard__search__btn uk-button uk-button-default uk-position-right"></button>
+                        </div>
+                    </div>
+                    <div class="tintuc__asideCard__item">
+                        <ul class="uk-nav uk-nav-default tintuc__asideCard__nav">
+                            <li class="uk-active"><a href="#">Danh mục bài viết 1</a></li>
+                            <li><a href="#">Danh mục bài viết 2</a></li>
+                            <li><a href="#">Danh mục bài viết 3</a></li>
+                            <li><a href="#">Danh mục bài viết 4</a></li>
+                            <li><a href="#">Danh mục bài viết 5</a></li>
+                            <li><a href="#">Danh mục bài viết 6</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
